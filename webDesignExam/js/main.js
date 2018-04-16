eea3b7d7106db1c7f3177920c69871fb44f0a2e6
vvesper <vanrych@umich.edu>
2018-04-16 17:23:24 -0400

    commit

--- webDesignExam/js/main.js
+++ webDesignExam/js/main.js
@@ -1,8 +1,12 @@
 function main() {
-	$('.img1').showImage();
-  $'.img2').showImage();
-  $('hideImage-button').on('click', function() {
-    $(this).toggleClass('hide');
-    $(this).next().slideToggle(400);
+  $('.hideImage-button').on('click', function() {
+    $('.img1').hide();
+    $('.img2').hide();
+    // $(this).next().slideToggle(400);
   });
+  $('.showImage-button').on('click', function() {
+    $('.img1').show();
+    $('.img2').show();
+    // $(this).next().slideToggle(400);
+  })
 }
